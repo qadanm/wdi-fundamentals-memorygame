808c4716868ad0b87bc7564ad6e7d99465ad3bb7
Muhammed Qadan <47371261+qadanm@users.noreply.github.com>
2019-07-04 00:30:36 -0700

    Add files via upload
    added game logic

---
+++ memorygame/js/main.js
@@ -0,0 +1,15 @@
+console.log("up and running");
+
+
+var cardOne = "queen";
+
+console.log("user flipped" + " " + cardOne);
+
+var cardTwo = "king";
+
+var cardThree = "queen";
+
+var cardFour = "king";
+
+
+console.log("user flipped" + " " + cardThree);
